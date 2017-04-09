b10d6b1c60dbe6b41c18796ad915344fa32c9bec
Masayoshi Kitamura <mak8818@gmail.com>
2017-04-09 21:46:11 +0000

    task user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+ 
   def show # 追加
    @user = User.find(params[:id])
   end
@@ -11,43 +11,30 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # ここを修正
     else
       render 'new'
     end
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-end
-class UsersController < ApplicationController
   
-  def show # 追加
-   @user = User.find(params[:id])
+  def edit
   end
   
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+  def update
+    if current_user.update(user_params)
+      redirect_to edit_user_path , notice: 'プロフィールを編集しました'
     else
-      render 'new'
+      render 'edit'
     end
   end
 
   private
 
+
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:place)
   end
 end
